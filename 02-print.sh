c014501be46124211787ab76f11b50d9d8363974
prasad <prasadpamidi579@gmail.com>
2022-01-27 02:00:19 +0530

    Printing colours in bold

--- 02-print.sh
+++ 02-print.sh
@@ -3,3 +3,6 @@
 echo WEAR MASK PROPERLY
 
 echo -e "\e[31mRed\e[32mGreen\e[33mYellow\e[34mBlue\e[35mMagenta\e[36mCyan\[0m"
+
+# the same color can print with more bold
+echo -e "\e[1;31mRed\e[1;32mGreen\e[1;33mYellow\e[1;34mBlue\e[1;35mMagenta\e[1;36mCyan\[0m"
