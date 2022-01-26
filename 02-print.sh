4408bde5669a8b8fc1db69e6792129719ae460f5
prasad <prasadpamidi579@gmail.com>
2022-01-27 02:11:45 +0530

    New Line and New Tab

--- 02-print.sh
+++ 02-print.sh
@@ -6,3 +6,10 @@ echo -e "\e[31mRed\e[32mGreen\e[33mYellow\e[34mBlue\e[35mMagenta\e[36mCyan\[0m"
 
 # the same color can print with more bold
 echo -e "\e[1;31mRed\e[1;32mGreen\e[1;33mYellow\e[1;34mBlue\e[1;35mMagenta\e[1;36mCyan\[0m"
+
+#for new line and tab
+# new line \n
+#new Tab \t
+
+echo -e "line1\nLine2"
+echo -e "word1\t\tword2"
