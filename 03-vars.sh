4c3ec2e81804e48100114ba6eee366ec01b77d5c
prasad <prasadpamidi579@gmail.com>
2022-01-27 22:07:28 +0530

    variable topic in command subtution

--- 03-vars.sh
+++ 03-vars.sh
@@ -11,3 +11,7 @@ echo Good morning, today date is $DATE
 ##command substution
 DATE=$(date +%F)
 echo Good morning, today date is $DATE
+
+##arthimatic substution
+EXPR1=$((2+4*5*6\5))
+echo EXPR1 OUTPUT = $EXPR1
