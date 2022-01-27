2d498f6c4e36ec41962785bd3cc105245d486b16
prasad <prasadpamidi579@gmail.com>
2022-01-27 22:03:03 +0530

    variable topic in command subtution

--- 03-vars.sh
+++ 03-vars.sh
@@ -5,7 +5,7 @@ student_name="Prasad"
 echo student_name = $student_name
 echo student_name = ${student_name}
 
-DATE=27-01-2022
+DATE=2022-01-27
 echo Good morning, today date is $DATE
 
 ##command substution
