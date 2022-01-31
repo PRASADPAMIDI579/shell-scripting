3ea09051726fc560a5268636a204470e37843444
prasad <prasadpamidi579@gmail.com>
2022-01-31 19:48:27 +0530

    frontend nginx instalation

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+if [ -f component/$1.sh ]; then
+  bash component/$1.sh
+  else
+    echo -e "\e[1;31mInvalid Input\e[0m"
+    echo -e "\e[1;33mAvailable Inputs - frontend|MongoDB|catalouge|redis|user|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
+    
+fi
