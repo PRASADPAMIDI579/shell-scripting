aba0d3be10e51d83168616876fce6e4bc0c828b2
prasad <prasadpamidi579@gmail.com>
2022-02-02 00:09:51 +0530

    downloading the frontend content tmp

--- roboshop-project/component/frontend.sh
+++ roboshop-project/component/frontend.sh
@@ -38,5 +38,5 @@ echo "installing nginx"
 yum install nginx -y >/tmp/roboshop.log
 
 echo "Download frontend content"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop.log
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>/tmp/roboshop.log
 
