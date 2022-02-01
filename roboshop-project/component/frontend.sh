df18ba87dfa963944c0e583a20ffe59f78c372c1
prasad <prasadpamidi579@gmail.com>
2022-02-02 00:45:17 +0530

    downloading the frontend content (clean,extract the content)

--- roboshop-project/component/frontend.sh
+++ roboshop-project/component/frontend.sh
@@ -50,3 +50,10 @@ yum install nginx -y &>>$LOG_FILE
 
 echo "Download frontend content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+
+echo "clean the old content"
+rm -rf /usr/share/nginx/html/* &>>LOG_FILE
+
+echo"Extract the front content"
+/usr/share/nginx/html/ $>>LOG_FILE
+unzip /tmp/frontend.zip $>>LOG_FILE
