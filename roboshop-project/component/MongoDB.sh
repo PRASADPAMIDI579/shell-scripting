4e66603087fdf220bcbffd58d45774763905ef2a
prasad <prasadpamidi579@gmail.com>
2022-02-10 23:09:49 +0530

    MongoDB script

--- roboshop-project/component/MongoDB.sh
+++ roboshop-project/component/MongoDB.sh
@@ -49,3 +49,21 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 echo " Instaling the MongoDB "
 yum install -y mongodb-org&>>LOG_FILE
 
+echo " update mongoDB config file"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
+
+echo " start database"
+systemctl enable mongod  &>>LOG_FILE
+systemctl start mongod  &>>LOG_FILE
+
+echo " download schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+echo " extract schema"
+cd /tmp
+unzip -o mongodb.zip  &>>LOG_FILE
+
+echo " load schema "
+cd mongodb-main
+mongo < catalogue.js  &>>LOG_FILE
+mongo < users.js  &>>LOG_FILE
