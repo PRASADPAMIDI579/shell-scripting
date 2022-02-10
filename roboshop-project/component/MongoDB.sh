5bceb5bea2d6d68c3bd55d5769dd550ec106c0ae
prasad <prasadpamidi579@gmail.com>
2022-02-10 22:45:09 +0530

    MongoDB

--- roboshop-project/component/MongoDB.sh
+++ roboshop-project/component/MongoDB.sh
@@ -47,5 +47,5 @@ echo "Downloading the MongoDB repo file"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 
 echo " Instaling the MongoDB "
-yum install -y mongodb-org
+yum install -y mongodb-org&>>LOG_FILE
 
