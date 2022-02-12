759cf41ea1a2d1259a55fb39ac0f1395155d26a2
prasad <prasadpamidi579@gmail.com>
2022-02-12 20:05:44 +0530

    create-ec2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -5,5 +5,5 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 if [ -z "${AMI_ID}" ]; then
   echo -e "unable to find image AMI_ID"
   else
-    echo -e "AMI_ID = ${AMI_ID}"
+    echo -e "\e[1;32mAMI_ID = ${AMI_ID}\e[0m"
     fi
