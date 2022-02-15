40c744128cd47d5159fde8286f13406a90eb5fcf
prasad <prasadpamidi579@gmail.com>
2022-02-15 23:51:52 +0530

    create-ec2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -9,8 +9,9 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 
 if [ -z "${AMI_ID}" ]; then
   echo -e "\e[1;34mUnable to find image AMI_ID\e[0m"
+  exit
   else
     echo -e "\e[1;33mAMI ID = ${AMI_ID}\e[0m"
   fi
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specfications "ResourceType=instance,Tags=[{key=Name,value=${INSTANCE_NAME}}]"
