6bf2984172bf150b467752e73d93493219f322f8
prasad <prasadpamidi579@gmail.com>
2022-02-15 23:21:07 +0530

    create-ec2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text
+
+if [ -z "${AMI_ID}" ]; then
+  echo "unable to find image AMI_ID"
+  fi
