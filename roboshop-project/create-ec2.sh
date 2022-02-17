b9279e130086ec3f007b1836919c91abdf211b8a
prasad <prasadpamidi579@gmail.com>
2022-02-18 01:26:22 +0530

    create-ec2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -18,15 +18,17 @@ if [ -z "${AMI_ID}" ]; then
 PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=frontend --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
 if [ -z "${PRIVATE_IP}" ]; then
+
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Aws-Practice Allow-access --query "SecurityGroups[*].GroupId" --output text)
 
-if [ -z "${SG_ID}" ];then
-  echo -e "\e[1;32m Security Group does not exit"
-  exist 1
+  if [ -z "${SG_ID}" ];then
+    echo -e "\e[1;32m Security Group does not exit"
+    exist 1
   fi
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
 
-else
-  echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
-  exit
+    aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
+
+  else
+    echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
+    exit
 fi
