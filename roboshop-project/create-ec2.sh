89c74444c707e6e6146454ac21e0030eed3d031e
prasad <prasadpamidi579@gmail.com>
2022-02-18 00:29:35 +0530

    create-ec2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -15,6 +15,12 @@ if [ -z "${AMI_ID}" ]; then
     echo -e "\e[1;33mAMI ID = ${AMI_ID}\e[0m"
   fi
 
-aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANCE_NAME}
+PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=frontend --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
+if [ -z "${PRIVATE_IP}" ]; then
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
+
+else
+  echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
+  exit
+fi
