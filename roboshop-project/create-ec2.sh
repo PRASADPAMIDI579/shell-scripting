8c97cbc9cba146a29ffa89b18a49feaf7dcb1fd6
prasad <prasadpamidi579@gmail.com>
2022-02-20 01:45:35 +0530

    create-ec2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -21,12 +21,21 @@ if [ -z "${AMI_ID}" ]; then
 PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANCE_NAME} --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
 if [ -z "${PRIVATE_IP}" ]; then
+  SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=Aws-Practice --query "SecurityGroups[*].GroupId" --output text)
+   if [ -z "${SG_ID}" ]; then
+     echo -e "\e[1;33m Security Group Aws-Practice does not exists"
+     exit
+     fi
 
-    aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,
-    Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,
+ags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
+ --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}"
+ --security-group-ids "${SG_ID}"
+
+
       echo -e "\e[1m Instance Created"
-  #else
-   # echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
-    #exit
+  else
+    echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
+    exit
 fi
 
