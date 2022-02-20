2d71b0d3618dcb7abf952d4ae32f7d46948fd5f5
prasad <prasadpamidi579@gmail.com>
2022-02-20 23:28:05 +0530

    create-ec2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -15,4 +15,5 @@ if [ -z "${AMI_ID}" ]; then
     echo -e "\e[1;33m AMI ID = ${AMI_ID}\e[0m"
   fi
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,
+Value=${INSTANCE_NAME}}]"
