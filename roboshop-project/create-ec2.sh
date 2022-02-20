8555596f9556f9cf786ce8aaeba7dd909cc5bfce
prasad <prasadpamidi579@gmail.com>
2022-02-20 23:01:51 +0530

    create-ec2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -1,40 +1,6 @@
 #!/bin/bash
 
-#LOG=/tmp/instance-create.log
-#rm -f $LOG
-
-INSTANCE_NAME=$1
-if [ -z "${INSTANCE_NAME}" ]; then
-  echo -e "\e[1;31mInstance Name argument is needed\e[0m"
-  exit
-  fi
-
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
-
+AMI_ID= $(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 if [ -z "${AMI_ID}" ]; then
-  echo -e "\e[1;34mUnable to find image AMI_ID\e[0m"
-  exit
-  else
-    echo -e "\e[1;33mAMI ID = ${AMI_ID}\e[0m"
+  echo "unable to find AMI_ID"
   fi
-
-PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${INSTANCE_NAME} --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
-
-if [ -z "${PRIVATE_IP}" ]; then
-  SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=Aws-Practice --query "SecurityGroups[*].GroupId" --output text)
-   if [ -z "${SG_ID}" ]; then
-     echo -e "\e[1;33m Security Group Aws-Practice does not exists"
-     exit
-     fi
-
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,
-Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"
- --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}"
-
-
-    #echo -e "\e[1m Instance Created"
-  else
-    echo "Instance ${INSTANCE_NAME} is already exists, Hence not creating"
-    exit
-fi
-
