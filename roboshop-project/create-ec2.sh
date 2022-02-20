e638133e77c9f32108dc8fdf0a5fed527c3a3e84
prasad <prasadpamidi579@gmail.com>
2022-02-20 23:58:42 +0530

    create-ec2

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -15,5 +15,7 @@ if [ -z "${AMI_ID}" ]; then
     echo -e "\e[1;33m AMI ID = ${AMI_ID}\e[0m"
   fi
 
+aws ec2 describe-instances --filters Name=tag:Name,Values={INSTANCE_NAME} --query 'Reservations[*].Instances[*].{PrivateIpAddress}'
+
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,
 Value=${INSTANCE_NAME}}]"
