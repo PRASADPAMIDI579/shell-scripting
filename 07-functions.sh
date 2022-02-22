de5f4d4ebff0bd66c19d395e37d1397755c2a417
prasad <prasadpamidi579@gmail.com>
2022-02-23 00:14:33 +0530

    functions

--- 07-functions.sh
+++ 07-functions.sh
@@ -2,8 +2,26 @@
 
 # declare a function
 SAMPLE() {
+
+  a=20
+  b=20
+  echo " WELCOME TO DEVOPS TRAINING "
+  echo " WELCOME TO DEVOPS TRAINING "
+  echo " WELCOME TO DEVOPS TRAINING "
   echo " WELCOME TO DEVOPS TRAINING "
 }
 
+#main program
 #call the function
+a=10
 SAMPLE
+SAMPLE
+b=10
+
+SAMPLE1(){
+  echo First Argument in Function =$1
+}
+
+echo First argument in main program =$1
+SAMPLE1 ABC
+
