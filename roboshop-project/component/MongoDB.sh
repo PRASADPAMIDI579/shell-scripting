7b55391d72d0b49a97cf1acaa4e99d8f0a4b8efd
prasad <prasadpamidi579@gmail.com>
2022-02-23 00:48:39 +0530

    mongoDB

--- roboshop-project/component/MongoDB.sh
+++ roboshop-project/component/MongoDB.sh
@@ -45,25 +45,32 @@ source component/common.sh
 
 echo "Downloading the MongoDB repo file"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+STAT $?
 
 echo " Instaling the MongoDB "
 yum install -y mongodb-org&>>LOG_FILE
+STAT $?
 
 echo " update mongoDB config file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
+STAT $?
 
 echo " start database"
 systemctl enable mongod  &>>LOG_FILE
 systemctl start mongod  &>>LOG_FILE
+STAT $?
 
 echo " download schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+STAT $?
 
 echo " extract schema"
 cd /tmp
 unzip -o mongodb.zip  &>>LOG_FILE
+STAT $?
 
 echo " load schema "
 cd mongodb-main
 mongo < catalogue.js  &>>LOG_FILE
 mongo < users.js  &>>LOG_FILE
+STAT $?
