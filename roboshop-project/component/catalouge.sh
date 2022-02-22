c535da11d81280cd8eca9b5d9d2f8c15e67c59b2
prasad <prasadpamidi579@gmail.com>
2022-02-23 01:11:02 +0530

    catalouge

--- roboshop-project/component/catalouge.sh
+++ roboshop-project/component/catalouge.sh
@@ -49,45 +49,56 @@
 
 source component/common.sh
 
-echo " setuo nodejs.repo"
+echo " setup nodejs.repo"
 curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -  &>>LOG_FILE
+STAT $?
 
 echo " installing nodejs "
 yum install nodejs gcc-c++ -y
+STAT $?
 
 echo " create app user"
 useradd roboshop &>>LOG_FILE
+STAT $?
 
 echo " download the catalouge code "
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG_FILE
-
+STAT $?
 
 echo " extract catalouge file"
-cd /tmp/
+cd /tmp
 unzip -o /tmp/catalouge.zip &>>LOG_FILE
+STAT $?
 
 echo " clean old content "
 rm -rf /home/roboshop/catalouge
+STAT $?
 
 echo " copy catolouge content "
 cp -r catalouge-main /home/roboshop/catalouge &>>LOG_FILE
+STAT $?
 
 echo " install nodeJS dependencies "
 npm install &>>LOG_FILE
+STAT $?
 
 chown roboshop:roboshop /home/roboshop/ -R &>>LOG_FILE
+STAT $?
 
 echo " update systemd file"
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
 /home/roboshop/catalouge/systemd.service &>>LOG_FILE
+STAT $?
 
 echo "setup catalouge SystemD file"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>LOG_FILE
+STAT $?
 
 echo " start catalouge"
 systemctl daemon-reload &>>LOG_FILE
 systemctl start catalogue &>>LOG_FILE
 systemctl enable catalogue &>>LOG_FILE
+STAT $?
 
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
-
+STAT $?
