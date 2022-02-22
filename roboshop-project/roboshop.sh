7f1f3ff97847c5b259ece88f8011c7d27edd8932
prasad <prasadpamidi579@gmail.com>
2022-02-22 22:53:22 +0530

    roboshop.sh

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo -e "\e[1;33m you shouls be a root user to execute the script \e[0m"
+  exit
+  fi
+
 if [ -f component/$1.sh ]; then
   bash component/$1.sh
   else
